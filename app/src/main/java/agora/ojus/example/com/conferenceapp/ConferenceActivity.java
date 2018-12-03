82b1cef4d36cc3cf64b59ea8b6817b0730751567
Ojus Save <ojas931992@gmail.com>
2018-12-02 19:44:45 -0800

    some minor fixes

--- app/src/main/java/agora/ojus/example/com/conferenceapp/ConferenceActivity.java
+++ app/src/main/java/agora/ojus/example/com/conferenceapp/ConferenceActivity.java
@@ -1,7 +1,11 @@
 package agora.ojus.example.com.conferenceapp;
 
 import android.Manifest;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,6 +14,9 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.util.Random;
 
 import io.agora.rtc.Constants;
 import io.agora.rtc.IRtcEngineEventHandler;
@@ -23,18 +30,22 @@ public class ConferenceActivity extends AppCompatActivity {
 
     private RtcEngine mRtcEngine;
     private IRtcEngineEventHandler mRtcEventHandler ;
+    public static AudioManager audioManager;
     private static final int PERMISSION_REQ_ID = 22;
 
     private static final String[] PERMISSIONS = {
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.CAMERA
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
     };
 
     public void init(){
         initRtcEngine();
         setupLocalVideo();
         joinChannel();
-
+        mute();
+        checkVol();
     }
 
     @Override
@@ -45,6 +56,16 @@ public class ConferenceActivity extends AppCompatActivity {
         if (checkPermissions()) {
             Log.i("PERMISSIONS_EXIST", "all permissions exist");
             init();
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager.isNotificationPolicyAccessGranted()) {
+            audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        } else {
+            // Ask the user to grant access
+            Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+            //startActivityForResult(intent);
         }
     }
 
@@ -192,5 +213,34 @@ public class ConferenceActivity extends AppCompatActivity {
         leaveChannel();
         RtcEngine.destroy();
         mRtcEngine = null;
+    }
+
+    public void mute() {
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
+        if (currentVolume == 0) {
+            Toast.makeText(getApplicationContext(), "Press unmute!", Toast.LENGTH_SHORT).show();
+        } else {
+            audioManager.setStreamVolume(
+                    AudioManager.STREAM_VOICE_CALL, // Stream type
+                    0, // Index
+                    AudioManager.FLAG_SHOW_UI // Flags
+            );
+        }
+    }
+    public void checkVol()
+    {
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
+        if (currentVolume != 0) {
+            Toast.makeText(getApplicationContext(), "Press Mute!", Toast.LENGTH_SHORT).show();
+        } else {
+            Random mRandom = new Random();
+            int media_max_volume = audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL);
+            int random_volume = mRandom.nextInt(((media_max_volume - 0) + 1) + 0);
+            audioManager.setStreamVolume(
+                    AudioManager.STREAM_VOICE_CALL, // Stream type
+                    random_volume, // Index
+                    AudioManager.FLAG_SHOW_UI // Flags
+            );
+        }
     }
 }
